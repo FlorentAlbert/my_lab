801fab821ceb802b093cba45ea2d904c07640da4
Florent ALBERT <florent.albert17@gmail.com>
2018-04-16 12:12:11 +0200

    Ménage + favicon

--- app/app.js
+++ app/app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 // Gestion d'erreurs
 var notFound = require('./errors/notFound');
 
 // Routes
-var indexRouter = require('./index');
+var indexRouter = require('./site/index');
 
 var app = express();
 
@@ -22,9 +23,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+
+// Routes
 app.use('/', indexRouter);
-
-
 app.use(notFound);
 
 module.exports = app;
